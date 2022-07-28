cee0093600db91ead12fa2fa7712e42164c0cc97
ZakiHD14 <zaki0530@hotmail.com>
2022-07-28 11:03:29 -0400

    added edit option for users

--- src/main/java/com/revature/controllers/UserController.java
+++ src/main/java/com/revature/controllers/UserController.java
@@ -3,12 +3,18 @@ package com.revature.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.revature.exceptions.UserNotFoundException;
 import com.revature.models.User;
 import com.revature.services.UserService;
 
@@ -38,5 +44,35 @@ public class UserController {
 	void addUser(@RequestBody User user) {
 		us.addUser(user);
 	}
+	
+	@PutMapping("/users")
+	public User updateUser(@RequestBody User user) throws UserNotFoundException {
+		return us.updateUser(user);
+	}
+	
+	@DeleteMapping("/users/{id}")
+	void deleteUser(@PathVariable(value="id") int id) throws UserNotFoundException {
+		us.deleteUser(id);
+		
+	}
+	
+	@GetMapping("/users/{id}")
+	public ResponseEntity<User> getUserById(@PathVariable ("id")int id){
+	
+			try {
+				
+				User user = us.getUserById(id);
+			return new ResponseEntity<>(user, HttpStatus.OK);
+			
+			}
+			catch (UserNotFoundException e) {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+				
+			}
+	}
+	
+	
+	
+	
 
 }
--- src/main/java/com/revature/services/UserService.java
+++ src/main/java/com/revature/services/UserService.java
@@ -10,30 +10,41 @@ import com.revature.repositories.UserRepository;
 
 @Service
 public class UserService {
-	
+
 	private UserRepository ur;
-	
-	public UserService (UserRepository ur) {
+
+	public UserService(UserRepository ur) {
 		super();
 		this.ur = ur;
 	}
-	
-	public List<User> getUsers(){
+
+	public List<User> getUsers() {
 		return ur.findAll();
 	}
-	
+
 	public User getByUsername(String username) {
 		return ur.findUserByUsername(username);
 	}
-	
-	
+
 	public User addUser(User user) {
 		return ur.save(user);
 	}
-	
+
 	public User getUserById(int id) throws UserNotFoundException {
 		User user = ur.findById(id).orElseThrow(() -> new UserNotFoundException());
 		return user;
+	}
+
+	public void deleteUser(int id) throws UserNotFoundException {
+		User user = ur.findById(id).orElseThrow(() -> new UserNotFoundException());
+		ur.delete(user);
+
+	}
+
+	public User updateUser(User user) throws UserNotFoundException {
+
+		return ur.save(user);
+
 	}
 
 }
