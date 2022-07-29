32a09e1b018f6e7e77d82beaa2d0802d01ecf23d
sz1557 <zscjswhk@gmail.com>
2022-07-29 15:53:00 -0500

    attempt fix

--- src/main/java/com/revature/util/CorsFix.java
+++ src/main/java/com/revature/util/CorsFix.java
@@ -1,18 +1,17 @@
 package com.revature.util;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Component
+@Configuration
+@EnableWebMvc
 public class CorsFix implements WebMvcConfigurer{
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://pomtreesbucket.s3-website-us-east-1.amazonaws.com", 
-							"http://localhost:4200")
-			.allowedHeaders("*")
-			.exposedHeaders("Authorization")
-			.allowedMethods("GET", "OPTIONS", "PUT", "POST", "DELETE");
+			.allowedOrigins("http://pomtreesbucket.s3-website-us-east-1.amazonaws.com",
+							"http://localhost:4200").allowedMethods("GET", "OPTIONS", "PUT", "POST", "DELETE");
 	}
 }
