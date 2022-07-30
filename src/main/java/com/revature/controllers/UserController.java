6c580babad6d9e90dfa568341e00eacc430eaf65
EFerri222 <eferri222@gmail.com>
2022-07-30 10:09:24 -0400

    Users can update their profile

--- src/main/java/com/revature/controllers/UserController.java
+++ src/main/java/com/revature/controllers/UserController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.revature.exceptions.UserNotFoundException;
 import com.revature.models.User;
 import com.revature.services.UserService;
 
@@ -38,5 +40,10 @@ public class UserController {
 	void addUser(@RequestBody User user) {
 		us.addUser(user);
 	}
+	
+	@PutMapping("/users")
+	public User updateUser(@RequestBody User user) throws UserNotFoundException {
+		return us.updateUser(user);
+  }
 
 }
