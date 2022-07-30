9c5fd35d353498a3493c5f3d23512f9af15f6490
sz1557 <zscjswhk@gmail.com>
2022-07-29 19:19:32 -0500

    edit blog

--- src/main/java/com/revature/controllers/BlogController.java
+++ src/main/java/com/revature/controllers/BlogController.java
@@ -3,13 +3,7 @@ package com.revature.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.revature.exceptions.BlogNotFoundException;
 import com.revature.models.Blog;
@@ -18,33 +12,38 @@ import com.revature.services.BlogService;
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class BlogController {
-	
+
 	private BlogService bs;
-	
+
 	public BlogController() {
 		super();
 	}
-	
+
 	@Autowired
 	public BlogController(BlogService bs) {
 		super();
 		this.bs = bs;
 	}
-	
+
 	@GetMapping("/blogs")
 	public List<Blog> getBlogs(){
 		return (List<Blog>) bs.getBlogs();
 	}
-	
+
 	@PostMapping("/blogs")
 	void addBlog(@RequestBody Blog blog) {
 		bs.addBlog(blog);
 	}
-	
+
 	@DeleteMapping("/blogs/{id}")
 	void deleteBlog(@PathVariable("id") int id) throws BlogNotFoundException {
 		bs.deleteBlog(id);
 	}
-	
+
+  @PutMapping("/blogs")
+  public Blog updateBlog(@RequestBody Blog blog) throws BlogNotFoundException {
+    return bs.updateBlog(blog);
+  }
+
 
 }
--- src/main/java/com/revature/services/BlogService.java
+++ src/main/java/com/revature/services/BlogService.java
@@ -10,37 +10,41 @@ import com.revature.repositories.BlogRepository;
 
 @Service
 public class BlogService {
-	
+
 	private BlogRepository br;
-	
-	
+
+
 	public BlogService(BlogRepository br) {
 		super();
 		this.br = br;
 	}
-	
+
 	public List<Blog> getBlogs(){
 		return br.findAll();
 	}
-	
+
 	public Blog getBlogById(int id) throws BlogNotFoundException {
 		Blog blog = br.findById(id).orElseThrow(() -> new BlogNotFoundException());
 		return blog;
 	}
-	
+
 	public Blog addBlog(Blog blog) {
 		return br.save(blog);
 	}
-	
+
 	public void deleteBlog(int id) throws BlogNotFoundException {
 		Blog blog = br.findById(id).orElseThrow(() -> new BlogNotFoundException());
 		br.delete(blog);
 	}
-	
+
 	public List<Blog> findBlogsByUserId(int userId){
 		List<Blog> blogs = br.findBlogsByUserId(userId);
 		return blogs;
-		
+
 	}
+
+  public Blog updateBlog(Blog blog) throws BlogNotFoundException {
+    return br.save(blog);
+  }
 
 }
